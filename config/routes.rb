009b46ab816f565314b2a9fe557f223e76731819
Mike Landon <landon.mike@gmail.com>
2021-07-16 23:16:43 +1000

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :commands
-  get 'homepage', to: 'homepage#index'
-  get 'homepage/about'
+  get '/about', to: 'homepage#about'
 
   root 'homepage#index'
 end
